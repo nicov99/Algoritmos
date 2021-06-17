383e004b903bd1f9c44ce4dff39a2c35db2ec91f
Nicolas Valdez <83422796+nicov99@users.noreply.github.com>
2021-06-17 17:43:06 -0300

    Add files via upload

--- TP 1/main2.cpp
+++ TP 1/main2.cpp
@@ -12,13 +12,14 @@ int main()
     cin >> c;
 
     disc = (b*b)-(4*a*c);
-    x1 = (-b+(sqrt(disc)))/(2*a);
-    x2 = (-b-(sqrt(disc)))/(2*a);
 
-    if (disc > 0){
+    if ((disc > 0)&&(a!=0)){
+        x1 = (-b+(sqrt(disc)))/(2*a);
+        x2 = (-b-(sqrt(disc)))/(2*a);
         cout << "Los valores de x son: " << endl << x1 << endl << x2 << endl;
     }
-      if (disc == 0){
+      if ((disc == 0)&&(a!=0)){
+        x1 = (-b+(sqrt(disc)))/(2*a);
         cout << "X tiene una unica solucion y es: " << x1 << endl;
     }
     if (disc < 0){
